e53b7325c00225f78e8457762fc7a52952ad6442
Cassius de Leeuwe <cadeleeuwe@gmail.com>
2019-07-31 22:07:31 +0700

    style: Add "how we work" animation

--- assets/js/new-main.js
+++ assets/js/new-main.js
@@ -1,9 +1,8 @@
 $(window).on('scroll', function(){
   $("#how-we-work .checklist li").each(function() {
-    
-    if (isScrolledIntoView(this)) {
-      console.log($(this))
-      $(this).delay(1000).addClass("active");
+    if (isOverCenterScreen(this, 150)) {
+      // console.log($(this))
+      $(this).addClass("active");
      }
   })
   
@@ -16,5 +15,16 @@ function isScrolledIntoView(elem) {
   var elemTop = $(elem).offset().top;
   var elemBottom = elemTop + $(elem).height();
 
-  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  console.log('docViewTop ' + docViewTop)
+  console.log('elemTop ' + elemTop)
+
+  // return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+function isOverCenterScreen(elem, offset) {
+  var windowCenter =  $(window).height() / 2;
+  var elemTop = elem.getBoundingClientRect().top;
+  console.log('-----------------');
+
+  return (windowCenter + offset) > elemTop;
 }
