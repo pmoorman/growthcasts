df79e488be0c23afebdbb5817fba2535f8e5fea1
Cassius de Leeuwe <cadeleeuwe@gmail.com>
2019-08-01 09:19:10 +0700

    style(WIP): Add animations

--- assets/js/new-main.js
+++ assets/js/new-main.js
@@ -1,11 +1,33 @@
 $(document).ready(function() {
+  affixHowWeHelpTitle();
+  animateIntroTitle();
+})
+
+function affixHowWeHelpTitle() {
   $('#how-we-help-sidebar').stickySidebar({
     topSpacing: 60,
     bottomSpacing: 60,
     containerSelector: '#how-we-help-container',
     innerWrapperSelector: '.sidebar__inner'
-});
-})
+  });
+}
+
+// TODO: Animate only when in viewport
+function animateIntroTitle() {
+  let titleEl = $('#intro .title');
+  let rightEl = $('#intro .title .right');
+  let words = ['leads', 'growth', 'revenue'], 
+      i = 0;
+
+  rightEl.on("animationend", function() {
+    titleEl.removeClass('active');
+  });
+
+  setInterval(function() {
+    rightEl.html(words[i=(i+1)%words.length]);
+    titleEl.addClass('active');
+  }, 3000);  
+}
 
 $(window).on('scroll', function(){
   
@@ -22,8 +44,31 @@ $(window).on('scroll', function(){
       $(this).addClass("active");
      }
   });
+
+  $(".slide-top, .slide-right").each(function() {
+    if (isOverCenterScreen(this, 150)) {
+      // console.log($(this))
+      $(this).addClass("active");
+     }
+  });
+
+  processSteps();
   
 });
+
+function processSteps() {
+  const first = $("#our-process .process-step:first-child");
+  const last = $("#our-process .process-step:last-child");
+  const line = $("#our-process .pct-line");
+  const maxLength = last.offset().top - first.offset().top;
+
+
+  let overScreen = isOverCenterScreen(first[0], 150);
+
+  if (overScreen && overScreen <= (maxLength + 50)) {
+    line.css('height', overScreen + 'px');
+  }
+}
 
 function isScrolledIntoView(elem) {
   var docViewTop = $(window).scrollTop();
@@ -35,7 +80,7 @@ function isScrolledIntoView(elem) {
   console.log('docViewTop ' + docViewTop)
   console.log('elemTop ' + elemTop)
 
-  // return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
 
 function isOverCenterScreen(elem, offset) {
@@ -43,5 +88,5 @@ function isOverCenterScreen(elem, offset) {
   var elemTop = elem.getBoundingClientRect().top;
   console.log('-----------------');
 
-  return (windowCenter + offset) > elemTop;
+  return (windowCenter + offset) > elemTop ? (windowCenter + offset) - elemTop : false ;
 }
