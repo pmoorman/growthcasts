5da3ae01158f24479ace39687419a7a67c1b4400
Cassius de Leeuwe <cadeleeuwe@gmail.com>
2019-08-02 14:18:55 +0700

    chore: Add footer links

--- assets/js/new-main.js
+++ assets/js/new-main.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   processSteps();
   onWindowsScroll();
   modalListener();
+  smoothScroll();
   _viewportAnimations();
 });
 
@@ -133,22 +134,41 @@ function modalListener() {
   });
 }
 
-
-// for (let i = 0; i < this._modalButton.length; i++) {
-//   this._modalButton[i].addEventListener("click", e => {
-//     e.preventDefault();
-
-//     let target = e.currentTarget.getAttribute("href");
-//     let modal = document.querySelector(target);
-
-//     this._isVisible = true;
-//     modal.classList.toggle("modal--visible");
-
-//     if (this._isVisible) {
-//       modal.addEventListener("click", e => {
-//         this._isVisible = false;
-//         e.target.classList.contains("jsModalBg") ? modal.classList.remove("modal--visible") : null;
-//       });
-//     }
-//   });
-// }
+function smoothScroll() {
+  // Select all links with hashes
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+}
