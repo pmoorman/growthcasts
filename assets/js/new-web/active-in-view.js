095a055372c2f641a676d0e92ade0ed58bb9521a
Cassius de Leeuwe <cadeleeuwe@gmail.com>
2021-04-09 19:00:30 +0700

    Add shadowbox animation

---
+++ assets/js/new-web/active-in-view.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const config = {
+    // How much of the element needs to be in viewport to trigger intersection (between 0 and 1)
+    root: null,
+    rootMargin: "-40% 0px -40% 0px",
+    threshold: 1,
+  };
+
+  // Create new Intersection Observer
+  const observer = new IntersectionObserver(handler, config);
+
+  function handler(entries, observer) {
+    for (entry of entries) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+        observer.unobserve(entry.target);
+      }
+    }
+  }
+
+  // Selecting multiple elements to observe
+  const elements = document.querySelectorAll(".active-in-view");
+  console.log({ elements });
+  // Adding observer on each element
+  elements.forEach((element) => {
+    observer.observe(element);
+  });
+});
